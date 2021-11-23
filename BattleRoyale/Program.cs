4384a438eddd9c8f032bfb5b5422e88114197512
Wouter <woutervandervelde2000@hotmail.com>
2021-11-23 17:45:43 +0100

    initial commit

---
+++ BattleRoyale/Program.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using System;
+using System.Threading.Tasks;
+
+namespace BattleRoyale
+{
+    class Program
+    {
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
