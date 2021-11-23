0259974d66a3988b1d04005509480c0ce068a824
Wouter <woutervandervelde2000@hotmail.com>
2021-11-23 20:01:42 +0100

    added commandhandler

--- BattleRoyale/Program.cs
+++ BattleRoyale/Program.cs
@@ -2,23 +2,28 @@
 using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
+using Module;
 
 namespace BattleRoyale
 {
 	class Program
 	{
 		private DiscordSocketClient _client;
+		private char _prefix;
 
 		public static void Main(string[] args)
 			=> new Program().MainAsync().GetAwaiter().GetResult();
 
 		public async Task MainAsync()
 		{
+			_prefix = Environment.GetEnvironmentVariable("prefix")[0];
 			_client = new DiscordSocketClient();
 			_client.Log += Log;
 
 			await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
 			await _client.StartAsync();
+
+			new CommandHandler(_client, new Discord.Commands.CommandService(), _prefix);
 
 			await Task.Delay(-1);
 		}
