e88c82a436b03cfc322c002870a1f8d3724d6114
Wouter <woutervandervelde2000@hotmail.com>
2021-11-23 17:57:56 +0100

    added working connection

--- BattleRoyale/Program.cs
+++ BattleRoyale/Program.cs
@@ -1,17 +1,26 @@
 ﻿using Discord;
+using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
 
 namespace BattleRoyale
 {
-    class Program
-    {
+	class Program
+	{
+		private DiscordSocketClient _client;
+
 		public static void Main(string[] args)
 			=> new Program().MainAsync().GetAwaiter().GetResult();
 
 		public async Task MainAsync()
 		{
+			_client = new DiscordSocketClient();
+			_client.Log += Log;
 
+			await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+			await _client.StartAsync();
+
+			await Task.Delay(-1);
 		}
 
 		private Task Log(LogMessage msg)
