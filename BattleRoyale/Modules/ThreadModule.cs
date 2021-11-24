2c6f8b7b5f64a210fddb2aaf0a549e35f49df1c2
Wouter <woutervandervelde2000@hotmail.com>
2021-11-24 20:49:19 +0100

    updated to expirmental Discord.NET Labs

---
+++ BattleRoyale/Modules/ThreadModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleRoyale.Modules
+{
+    public class ThreadModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("thread")]
+        [Alias("t")]
+        [Summary("Creates a thread")]
+        public async Task ThreadAsync()
+        {
+
+        }
+    }
+}
